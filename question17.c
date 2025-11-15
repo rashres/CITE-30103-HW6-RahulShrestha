d8eef8112b92fba41942306c758bf952f9db1246
rashres <rashres027@gmail.com>
2025-11-14 18:27:14 -0600

    18 done

---
+++ question17.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+    int marks;
+
+    printf("Enter marks: ");
+    scanf("%d", &marks);
+
+    if (marks >= 90) {
+        printf("A\n");
+    } 
+    else if (marks >= 80) {
+        printf("B\n");
+    }
+    else if (marks >= 70) {
+        printf("C\n");
+    }
+    else if (marks >= 60) {
+        printf("D\n");
+    }
+    else {
+        printf("F\n");
+    }
+
+    return 0;
+}
